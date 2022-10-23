e7b8ae024800f68248ca1074314c68d734d358b6
madiha1012 <madikhan2310>
2022-10-23 19:09:57 +0500

    SecondCommit_

--- main.dart
+++ main.dart
@@ -1,5 +1,40 @@
 void main() {
-  List mylist = ["Madiha", "Huzaifa", 12, 18, 78];
+  print("=======");
+
+  Map my_info = {"ID": 10145, "name": "Madiha Khan", "PhoneNo": 03358949494};
+  print(my_info);
+
+  print("=======");
+  print("Only Keys");
+  print(my_info.keys);
+  print("=======");
+  print("only values");
+  print(my_info.values);
+  print("=======");
+
+  print("Length of map");
+  print(my_info.length);
+  print("=======");
+
+  print("isEmpty method");
+  print(my_info.isEmpty);
+  print("=======");
+
+  print("isNotEmpty Method");
+  print(my_info.isNotEmpty);
+  print("=======");
+
+  print("Add methods");
+  my_info["City"] = "Karachi";
+  print(my_info);
+  print("=======");
+
+  my_info.putIfAbsent("Country", () => "Pakistan");
+  print(my_info);
+  print("=======");
+
+  
+/*  List mylist = ["Madiha", "Huzaifa", 12, 18, 78];
   print(mylist);
   print("=====");
 
@@ -31,5 +66,5 @@ void main() {
   print("Replace Range method");
   mylist.replaceRange(2, 5, [23, 54, 76]);
   print(mylist);
-  print("=====");
+  print("=====");*/
 }
